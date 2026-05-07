2ea92742937edb9d53f7a4b0629501a396aabdd8
shuruqsaud <you@example.com>
2026-05-07 20:05:29 +0300

    aa

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -50,9 +50,12 @@ class SharedResources {
     
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: Multiple threads might read and write simultaneously!
+       contextSwitchLock.lock();
+       try {
         contextSwitchCount++;
+       } finally {
+           contextSwitchLock.unlock();
+       }
     }
     
     // Method to increment completed process counter
