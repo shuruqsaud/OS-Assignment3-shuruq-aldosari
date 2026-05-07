8bbcc3945343231526d15ae4f29a86959446b2b3
shuruqsaud <you@example.com>
2026-05-07 20:08:06 +0300

    aaa

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -60,14 +60,23 @@ class SharedResources {
     
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
+        completedProcessLock.lock();
+        try {   
+            completedProcessCount++;
+        } finally {
+            completedProcessLock.unlock();
+        }
         completedProcessCount++;
     }
     
     // Method to add waiting time
     public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        waitingTimeLock.lock();
+        try {
+            totalWaitingTime += time;
+        } finally {
+            waitingTimeLock.unlock();
+        }
     }
     
     // Method to log execution
