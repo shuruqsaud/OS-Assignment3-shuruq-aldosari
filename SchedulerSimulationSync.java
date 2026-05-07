fe3c42dcb5235d49dad1b979df6f8da5d4037a74
shuruqsaud <you@example.com>
2026-05-07 20:23:10 +0300

    ai

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -115,9 +115,8 @@ class Process implements Runnable {
     
     @Override
     public void run() {
-        // TODO #3: Acquire CPU semaphore before executing
-        // This ensures only allowed number of processes run simultaneously
-        
+       try{
+        sharedResources.cpuSemaphore.acquire();
         try {
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
@@ -178,8 +177,7 @@ class Process implements Runnable {
             System.out.println();
             
         } finally {
-            // TODO #4: Release CPU semaphore here
-            // Always release in finally block to prevent deadlocks!
+            sharedResources.cpuSemaphore.release();
         }
     }
     
